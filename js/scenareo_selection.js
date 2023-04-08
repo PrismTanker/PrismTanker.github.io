8abe9a18217e87d5f5710a903adf8d091d3d76b1
PrismTanker <69773486+PrismTanker@users.noreply.github.com>
2023-04-09 09:48:46 +1000

    cleaned up a little

--- js/scenareo_selection.js
+++ js/scenareo_selection.js
@@ -1,5 +1,4 @@
 const SELECTIONSIZE = 3
-
 var active_selection = {};
 
 function clear_prompt() {
@@ -9,12 +8,11 @@ function clear_prompt() {
 } 
 
 function init_tool() {
-    let theChosenOnes = {};
     for (set in cardData) { // cardData defined in load_data.js
         if (set != PATH){ // PATH defined in load_data.js
             // Build weighted set of card indexes
             let selectionIndicies = [];
-            dset = cardData[set]
+            dset = cardData[set];
             cardSet = dset.cards;
             for (cardIndex in cardSet) {
                 for (let i = 0; i < cardSet[cardIndex].selectCount; i++){
@@ -23,16 +21,18 @@ function init_tool() {
             }
 
             // Select distinct cards (mimicking random card draw as close as possible)
-            theChosenOnes[set] = [];
-            while (theChosenOnes[set].length < SELECTIONSIZE){
+            theChosenOnes = [];
+            while (theChosenOnes.length < SELECTIONSIZE){
                 if (selectionIndicies.length <= 0) {
+                    // Implies a fucky wucky with card selection restrictions
                     throw new Error("Selected Card List Exhausted (This should not happen)");
                 }
+
                 choice = Math.floor(Math.random() * selectionIndicies.length);
                 candidate = selectionIndicies[choice];
                 selectionIndicies.splice(choice, 1); // Remove candidate from list
-                if (!theChosenOnes[set].includes(candidate)) {
-                    theChosenOnes[set].push(candidate);
+                if (!theChosenOnes.includes(candidate)) {
+                    theChosenOnes.push(candidate);
                 }
             }
 
@@ -40,12 +40,12 @@ function init_tool() {
             rootImgFolder = cardData.path;
             setFolder = dset.path;
             active_selection[set] = [pathJoin([rootImgFolder, setFolder, dset.default.img])];// Always include base card
-            for (i in theChosenOnes[set]) {
-                active_selection[set].push(pathJoin([rootImgFolder, setFolder, cardSet[theChosenOnes[set][i]].img]));
+            for (i in theChosenOnes) {
+                active_selection[set].push(pathJoin([rootImgFolder, setFolder, cardSet[theChosenOnes[i]].img]));
             }
 
             // Display cards
-            imBlocks = $('#'.concat(set, "_tool > .card_row img"))
+            imBlocks = $('#'.concat(set, "_tool > .card_row img")) // Bite me
             for (i in imBlocks) {
                 imBlocks[i].src = active_selection[set][i]
             }
