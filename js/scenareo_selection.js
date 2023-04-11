47776a83eb52d604130db11dcb3f75caadde95ab
PrismTanker <69773486+PrismTanker@users.noreply.github.com>
2023-04-11 14:59:32 +1000

    Selection tool fully operational

--- js/scenareo_selection.js
+++ js/scenareo_selection.js
@@ -1,15 +1,27 @@
 const SELECTIONSIZE = 3
-var active_selection = {};
+var activeSelection;
 
 function clear_prompt() {
+    $("#tool_reset").on("click", init_tool);
     $("#initial_prompt").css({"display": "none"});
     $("#the_tool").css({"display": "inline"});
     init_tool();
 } 
 
 function init_tool() {
-    for (set in cardData) { // cardData defined in load_data.js
-        if (set != PATH){ // PATH defined in load_data.js
+    // Reset tool visibility
+    $("#card_selector").css({"display": "inline"});
+    $("#results_display").css({"display": "none"});
+
+    //Reset prompts
+    $("#the_tool > h1:first-child").html("Eliminate cards by selecting them")
+    $("#tool_reset").siblings("h3").each(function (i) { // should contain a single element
+        $(this).html("You can reselect cards and start again at any time")
+    });
+
+    activeSelection = {"remainingSets": 0};
+    for (set in cardData) { // TODO should probably break this down into functions. // cardData defined in load_data.js
+        if (set != PATH){  // PATH defined in load_data.js
             // Build weighted set of card indexes
             let selectionIndicies = [];
             dset = cardData[set];
@@ -39,16 +51,77 @@ function init_tool() {
             // Build Card set
             rootImgFolder = cardData.path;
             setFolder = dset.path;
-            active_selection[set] = [pathJoin([rootImgFolder, setFolder, dset.default.img])];// Always include base card
+            activeSelection.remainingSets++;
+            activeSelection[set] = [{"selectionID": 0, "name": dset.default.name, "path": pathJoin([rootImgFolder, setFolder, dset.default.img])}];// Always include base card
             for (i in theChosenOnes) {
-                active_selection[set].push(pathJoin([rootImgFolder, setFolder, cardSet[theChosenOnes[i]].img]));
+                activeSelection[set].push({"selectionID": parseInt(i)+1, "name": cardSet[theChosenOnes[i]].name, "path": pathJoin([rootImgFolder, setFolder, cardSet[theChosenOnes[i]].img])}); //pathJoin defined in script.js
             }
 
             // Display cards
-            imBlocks = $('#'.concat(set, "_tool > .card_row img")) // Bite me
-            for (i in imBlocks) {
-                imBlocks[i].src = active_selection[set][i]
-            }
+            imgParent = '#'.concat(set, "_tool > .card_row")
+            $(imgParent.concat(" img")).each(function (i) { //Storing and iterating over $(selection) has iterable metadata and goes goofy, use each instead.
+                $(this).attr({ // Remember to wrap things in $ to use JQuery methods
+                    "src": activeSelection[set][i].path, // Realistically at this point I should be using a backend database but, uh, No.
+                    "alt": activeSelection[set][i].name,
+                    "data-selID": activeSelection[set][i].selectionID //Unique ID to map element back to internal representation
+                });
+                $(this).css({ // Reset card visibility
+                    "display": "inline",
+                    "opacity": 1
+                }); 
+            });
+            
+            // If not set up, configure event handlers while we are here. Apparantly its bad practice to add event handlers to children directly
+            $(imgParent).each(function (i) { // Should only contain one element
+                if(this.onclick == undefined) {
+                    (function (t,s) {
+                        t.onclick = function (event) { // Alternate click event add method, detectable by the above if statement but only allows one handler
+                            selectCard(event.target, s);
+                        }
+                    }) (this,set); // IIFE to capture current set value
+                }
+            });
         }                    
     }
 }
+
+function selectCard(card, set) {
+    if (activeSelection[set].length > 1) {
+        $(card).css({"display": "none"});
+        // Remove card from candidates
+        activeSelection[set] = activeSelection[set].filter(choice => choice.selectionID != $(card).attr('data-selID'));
+
+        if (activeSelection[set].length <= 1) {
+            // Store chosen card in results
+            $("#".concat(set, "_result > img:first-of-type")).attr({
+                "src": activeSelection[set][0].path, //If we overshoot one we deserve to error
+                "alt": activeSelection[set][0].name
+            })
+            
+            // Apply styling to selected card
+            $(card).siblings("img[data-selID=\"".concat(activeSelection[set][0].selectionID, "\"]")).each(function (i) { //Should only contain single element
+                $(this).css({
+                    "opacity": 0.5
+                });
+            });
+
+            activeSelection.remainingSets--;
+        }
+    }
+
+    if (activeSelection.remainingSets <= 0) {
+        displayResults()
+    }
+}
+
+function displayResults() {
+    $("#card_selector").css({"display": "none"});
+    $("#results_display").css({"display": "flex"});
+    $("#the_tool > h1:first-child").html("Selected Scenareo")
+    $("#tool_reset").siblings("h3").each(function (i) { // should contain a single element
+        $(this).html("Choose another Scenareo")
+    });
+
+}
+
+// TODO!!!!!!!!!!!!!!!!!!!! .css applies inline css. replace all calls with class modification and write stylesheets
